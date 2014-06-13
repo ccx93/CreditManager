023e709d1b83ca30659900a7bec82b82375999a0
Christ <clement105.c@gmail.com>
2014-06-13 17:51:21 +0700

    Update database handler and contract

--- src/com/ccx/creditmanager/DatabaseContract.java
+++ src/com/ccx/creditmanager/DatabaseContract.java
@@ -4,39 +4,73 @@ import android.provider.BaseColumns;
 
 public class DatabaseContract
 {
-	public DatabaseContract() {};
-	
-	public static abstract class Masters implements BaseColumns
+	public DatabaseContract()
 	{
-		public static final String TABLE_NAME = "master";
-		public static final String COLLUMN_NAME_ID = "id";
-		public static final String COLLUMN_NAME_NAME = "name";
+	};
 
-		public static final String TYPE_ID = "STRING";
-		public static final String TYPE_NAME = "TEXT";
+	public static final String	DATABASE_NAME		= "CreditMaster";
+	public static final int		DATABASE_VERSION	= 1;
+
+	private static final String	TEXT_TYPE			= " TEXT";
+	private static final String	INTEGER_TYPE		= " INTEGER";
+	private static final String	SEPARATOR			= ",";
+
+	public static abstract class TransactionMaster implements BaseColumns
+	{
+		public static final String	TABLE_NAME			= "master";
+		public static final String	COLUMN_NAME_NAME	= "name";
+
+		public static final String	TYPE_NAME			= TEXT_TYPE;
+
+		//formatteroff
+		public static final String	CREATE_TABLE		= 
+				"CREATE TABLE " + TABLE_NAME + " (" + 
+				_ID + "INTEGER PRIMARY KEY," 
+				+ COLUMN_NAME_NAME + TYPE_NAME + ")";
+		//formatteron
+
+		public static final String	DELETE_TABLE		= "DROP TABLE IF EXISTS " + TABLE_NAME;
+
 	}
-	
-	
+
 	public static abstract class Transactions implements BaseColumns
 	{
-		public static final String TABLE_NAME = "transaction";
-		public static final String COLLUMN_NAME_ID = "id";
-		public static final String COLLUMN_NAME_VALUE = "value";
-		public static final String COLLUMN_NAME_DATE = "date";
-		
-		public static final String TYPE_ID = "STRING";
-		public static final String TYPE_VALUE = "LONG";
-		public static final String TYPE_DATE = "DATE";
-	}
-	
-	public static abstract class MasterLink implements BaseColumns
-	{
-		public static final String TABLE_NAME = "transmaster";
-		public static final String COLLUMN_NAME_MASTERID = "masterid";
-		public static final String COLLUMN_NAME_TRANSACTIONID = "transactionid";
+		public static final String	TABLE_NAME			= "transaction";
+		public static final String	COLLUMN_NAME_VALUE	= "value";
+		public static final String	COLLUMN_NAME_DATE	= "date";
 
-		public static final String TYPE_MASTERID = "STRING";
-		public static final String TYPE_TRANSACTIONID = "STRING";
+		public static final String	TYPE_VALUE			= INTEGER_TYPE;
+		public static final String	TYPE_DATE			= INTEGER_TYPE;
+		
+		//formatteroff
+		public static final String	CREATE_TABLE		= 
+				"CREATE TABLE " + TABLE_NAME + " (" + 
+				_ID + "INTEGER PRIMARY KEY," +
+				COLLUMN_NAME_VALUE + TYPE_VALUE + SEPARATOR +
+				COLLUMN_NAME_DATE + TYPE_DATE + ")";
+		//formatteron
+
+		public static final String	DELETE_TABLE		= "DROP TABLE IF EXISTS " + TABLE_NAME;
 	}
-	
+
+	public static abstract class MasterAndTransaction implements BaseColumns
+	{
+		public static final String	TABLE_NAME					= "transmaster";
+		public static final String	COLLUMN_NAME_MASTERID		= "masterid";
+		public static final String	COLLUMN_NAME_TRANSACTIONID	= "transactionid";
+
+		public static final String	TYPE_MASTERID				= INTEGER_TYPE;
+		public static final String	TYPE_TRANSACTIONID			= INTEGER_TYPE;
+
+		//formatteroff
+		public static final String	CREATE_TABLE		= 
+				"CREATE TABLE " + TABLE_NAME + " (" + 
+				_ID + "INTEGER PRIMARY KEY," +
+				COLLUMN_NAME_MASTERID + TYPE_MASTERID + SEPARATOR +
+				COLLUMN_NAME_TRANSACTIONID + TYPE_TRANSACTIONID + ")";
+		//formatteron
+
+		public static final String	DELETE_TABLE				= "DROP TABLE IF EXISTS " + TABLE_NAME;
+	}
+
 }
--- src/com/ccx/creditmanager/DatabaseHandler.java
+++ src/com/ccx/creditmanager/DatabaseHandler.java
@@ -1,9 +1,28 @@
 package com.ccx.creditmanager;
 
-public class DatabaseHandler
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHandler extends SQLiteOpenHelper
 {
-	
-	// TODO create method body
-	public void saveDatabase();
-	public TransactionMaster loadDatabase();
+
+	public DatabaseHandler(Context context)
+	{
+		super(context, DatabaseContract.DATABASE_NAME, null, DatabaseContract.DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db)
+	{
+		db.execSQL(DatabaseContract.TransactionMaster.CREATE_TABLE);
+		db.execSQL(DatabaseContract.Transactions.CREATE_TABLE);
+		db.execSQL(DatabaseContract.MasterAndTransaction.CREATE_TABLE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+	{
+		// no plan to upgradeDB
+	}
 }
