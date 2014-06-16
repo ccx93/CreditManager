12abd1e2939a4f5c8058f7271789e1c7e3b86f84
Christ <clement105.c@gmail.com>
2014-06-16 16:18:10 +0700

    Commit blobs and resources

--- gen/com/ccx/creditmanager/R.java
+++ gen/com/ccx/creditmanager/R.java
@@ -8,13 +8,65 @@
 package com.ccx.creditmanager;
 
 public final class R {
+    public static final class array {
+        public static final int TabMenu=0x7f060000;
+    }
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+        /** 
+         Per the design guidelines, navigation drawers should be between 240dp and 320dp:
+         https://developer.android.com/design/patterns/navigation-drawer.html
+    
+         */
+        public static final int navigation_drawer_width=0x7f040002;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int drawer_shadow=0x7f020000;
+        public static final int ic_drawer=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+    }
+    public static final class id {
+        public static final int action_example=0x7f090007;
+        public static final int action_settings=0x7f090006;
+        public static final int drawer_layout=0x7f090000;
+        public static final int navigation_drawer=0x7f090001;
+        public static final int textView1=0x7f090002;
+        public static final int textView2=0x7f090004;
+        public static final int text_cardDueDate=0x7f090005;
+        public static final int text_cardName=0x7f090003;
+    }
+    public static final class layout {
+        public static final int activity_main=0x7f030000;
+        public static final int activity_summary_fragment=0x7f030001;
+        public static final int fragment_main=0x7f030002;
+        public static final int fragment_navigation_drawer=0x7f030003;
+    }
+    public static final class menu {
+        public static final int global=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int action_example=0x7f050007;
+        public static final int action_settings=0x7f050008;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f05000a;
+        public static final int navigation_drawer_close=0x7f050006;
+        public static final int navigation_drawer_open=0x7f050005;
+        public static final int title_activity_main=0x7f050001;
+        public static final int title_activity_summary_fragment=0x7f050009;
+        public static final int title_section1=0x7f050002;
+        public static final int title_section2=0x7f050003;
+        public static final int title_section3=0x7f050004;
     }
     public static final class style {
         /** 
@@ -38,10 +90,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f070001;
     }
 }
